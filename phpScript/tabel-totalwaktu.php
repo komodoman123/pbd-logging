fc3f349ab6d57390574088e89bebb8ff109b7122
komodoman123 <rifqi546@gmail.com>
2018-05-15 15:31:37 +0700

    asd
    asd

--- phpScript/tabel-totalwaktu.php
+++ phpScript/tabel-totalwaktu.php
@@ -18,10 +18,10 @@ $results=$getResults->fetchAll(PDO::FETCH_BOTH);
 foreach ($results as $row) {
   echo "<tr>";
   echo "<td>".$row['idProyek']."</td>";
-<<<<<<< HEAD
-=======
-  echo "<td>".$row['namaProyek']."</td>";  
->>>>>>> 9bde9b9fe4ded2ba0c2e4ee747fa58ff0cea7e9c
+
+
+  echo "<td>".$row['namaProyek']."</td>";
+
   echo "<td>".$row['totaljam']."</td>";
   //echo $row['idpegawai'].''.$row['namaPegawai'];
   echo '</tr>';
